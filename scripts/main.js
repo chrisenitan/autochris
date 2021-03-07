1f34c868e8cb11c411293f1741df6bc952c42915
chrisenitan <enitanchris@gmail.com>
2021-03-07 21:29:35 +0100

    working version... still need to make custom presets

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,11 @@ window.addEventListener("load", function(){
 
     var allChecks = document.querySelectorAll(".cols")
     for (var i = 0; i < allChecks.length; i++) {
+        //set initially to black on DOM
+        allChecks[i].style.backgroundColor = "black";
+        //attach listener to all divs
         document.querySelectorAll(".cols")[i].addEventListener("click", function(){
+            //get specific div ID
             var colId = this.id;
             //get initial color
             var initialColor = document.getElementById(colId).style.backgroundColor;
@@ -17,7 +21,7 @@ window.addEventListener("load", function(){
                 default:
                     document.getElementById(colId).style.backgroundColor = "black";
             }
-            //get new color
+            //get new color and log changes
             var newColor = document.getElementById(colId).style.backgroundColor;
             console.log(`Changed background for box ${colId} from ${initialColor} to ${newColor}`)
         
