c230138f8c5121964267d1295a95770f708823b6
chrisenitan <enitanchris@gmail.com>
2021-03-09 07:31:41 +0100

    pretty

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,33 @@
 //main js file
-window.addEventListener("load", function(){
-
-    //checked scripts
-    var allChecks = document.querySelectorAll(".cols")
-    for (var i = 0; i < allChecks.length; i++) {
-        //set initially to black on DOM
-        allChecks[i].style.backgroundColor = "black";
-        //attach listener to all divs
-        document.querySelectorAll(".cols")[i].addEventListener("click", function(){
-            //get specific div ID
-            var colId = this.id;
-            //get initial color
-            var initialColor = document.getElementById(colId).style.backgroundColor;
-            switch (initialColor){
-                case "black":
-                    document.getElementById(colId).style.backgroundColor = "white";
-                break;
-                case "white":
-                    document.getElementById(colId).style.backgroundColor = "black";
-                break;
-                default:
-                    document.getElementById(colId).style.backgroundColor = "black";
-            }
-            //get new color and log changes
-            var newColor = document.getElementById(colId).style.backgroundColor;
-            console.log(`Changed background for box ${colId} from ${initialColor} to ${newColor}`)
-        })
-      }
-
+window.addEventListener("load", function () {
+  //checked scripts
+  var allChecks = document.querySelectorAll(".cols")
+  for (var i = 0; i < allChecks.length; i++) {
+    //set initially to black on DOM
+    allChecks[i].style.backgroundColor = "black"
+    //attach listener to all divs
+    document
+      .querySelectorAll(".cols")
+      [i].addEventListener("click", function () {
+        //get specific div ID
+        var colId = this.id
+        //get initial color
+        var initialColor = document.getElementById(colId).style.backgroundColor
+        switch (initialColor) {
+          case "black":
+            document.getElementById(colId).style.backgroundColor = "white"
+            break
+          case "white":
+            document.getElementById(colId).style.backgroundColor = "black"
+            break
+          default:
+            document.getElementById(colId).style.backgroundColor = "black"
+        }
+        //get new color and log changes
+        var newColor = document.getElementById(colId).style.backgroundColor
+        console.log(
+          `Changed background for box ${colId} from ${initialColor} to ${newColor}`
+        )
+      })
+  }
 })
