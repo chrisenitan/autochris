07d744b76be75c051ecccb7eb2d92506a0c75469
chrisenitan <enitanchris@gmail.com>
2021-10-17 15:50:59 +0200

    promise asyncawait readline cli practise

--- scripts/mush.js
+++ scripts/mush.js
@@ -1,17 +1,43 @@
 //simple cli tool
+const readline = require("readline")
 const args = process.argv[2]
-const usageHint =
-  "\nUsage:\nmush <command>\n\nAll Commands:\nmush help: overview of commands\nmush date: returns the full dateTimeZone\nmush flipcoin: flips a coin and returns 'heads' or 'tails'"
+const usageHint = `Usage:
+  mush <command>\n
+  All Commands:
+  mush help: overview of commands
+  mush date: returns the full dateTimeZone
+  mush flipcoin: flips a coin and returns 'heads' or 'tails'
+  mush talk: asks a question and responds
+  `
+
+//general output logger
 let outer = (req) => {
   console.log(req)
+  process.exit(0)
 }
+
+//get and privide some user interation
+let talker = () =>
+  new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+    rl.question("Hi there, how are you?\n", (answer) => {
+      resolve(answer)
+      rl.close()
+    })
+  })
+
+//main process
 let processRequest = (args) => {
-  //sanitise
+  //sanitise input array
   var errMessage = ""
   if (process.argv.length != 3) {
     errMessage = "Invalid request recieved"
     args = "help"
   }
+  //engine
   switch (args) {
     case "help":
       outer(`mush CLI: ${process.cwd()}\n${errMessage}\n${usageHint}`)
@@ -26,6 +52,24 @@ let processRequest = (args) => {
         var coin = "Tails"
       }
       outer(coin)
+      break
+    case "talk":
+      /* talker()
+        .then((res) => {
+          outer(`${res}? That's one way to feel.`)
+        })
+        .catch((errMessage) => {
+          console.log(`Did not get a clear response`)
+        }) */
+      let nudge = async () => {
+        try {
+          const replyUser = await talker()
+          console.log(`${replyUser}? That's one way to feel.`)
+        } catch {
+          console.log(`Did not get a clear response`)
+        }
+      }
+      nudge()
       break
     default:
       outer(`Could not parse the request\n${usageHint}`)
