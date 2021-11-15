a022990c07913552592b3a5a2cc2dd04640a04cf
chrisenitan <enitanchris@gmail.com>
2021-11-15 17:01:20 +0100

    screenshot and file management

--- scripts/ott.js
+++ scripts/ott.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer")
+const fs = require("fs")
 const puppeteer = require("puppeteer")
 
 /* visit a page and take a screenshot
@@ -18,13 +19,26 @@ let ott = async (req) => {
 
   //try
   try {
+    //define attr
+    const sAtt = {}
     //foreach...reduce??
     await page.goto(req[0])
+    sAtt.fileName = await page.title()
+    await page.screenshot({
+      type: "png",
+      path: `screenshots/${sAtt.fileName}.png`,
+    })
     await browser.close()
+    //delete bloat files
+    fs.unlink(`screenshots/${sAtt.fileName}.png`, (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
   } catch (error) {
     console.log(error)
     await browser.close()
   }
 }
 
-ott(["https://www.google.com", "https://www.google.com"])
+ott(["https://www.google.com", "https://www.twitter.com"])
