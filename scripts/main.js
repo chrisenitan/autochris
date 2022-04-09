d857e126f56e9e2fbb9ac34e89278e3cccd18fb9
chrisenitan <enitanchris@gmail.com>
2022-04-09 19:18:34 +0200

    main: visual checked clean up

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-//main js file
+//listne and swicth colour on click each box
 window.addEventListener("load", function () {
   //checked scripts
   var allChecks = document.querySelectorAll(".cols")
@@ -6,46 +6,42 @@ window.addEventListener("load", function () {
     //set initially to black on DOM
     allChecks[i].style.backgroundColor = "black"
     //attach listener to all divs
-    document
-      .querySelectorAll(".cols")
-      [i].addEventListener("click", function () {
-        //get specific div ID
-        var colId = this.id
-        //get initial color
-        var initialColor = document.getElementById(colId).style.backgroundColor
-        switch (initialColor) {
-          case "black":
-            document.getElementById(colId).style.backgroundColor = "white"
-            break
-          case "white":
-            document.getElementById(colId).style.backgroundColor = "black"
-            break
-          default:
-            document.getElementById(colId).style.backgroundColor = "black"
-        }
-        //get new color and log changes
-        var newColor = document.getElementById(colId).style.backgroundColor
-        console.log(
-          `Changed background for box ${colId} from ${initialColor} to ${newColor}`
-        )
-      })
+    document.querySelectorAll(".cols")[i].addEventListener("click", function () {
+      //get specific div ID
+      var colId = this.id
+      //get initial color
+      var initialColor = document.getElementById(colId).style.backgroundColor
+      switch (initialColor) {
+        case "black":
+          document.getElementById(colId).style.backgroundColor = "white"
+          break
+        case "white":
+          document.getElementById(colId).style.backgroundColor = "black"
+          break
+        default:
+          document.getElementById(colId).style.backgroundColor = "black"
+      }
+      //get new color and log changes
+      var newColor = document.getElementById(colId).style.backgroundColor
+      console.log(`Changed background for box ${colId} from ${initialColor} to ${newColor}`)
+    })
   }
 })
 
-//custom brightness level for checked 
-let customLevel = (req) =>{
+//control buttons. setting default brightness levels
+let customLevel = (req) => {
   var checks = document.querySelectorAll(`.${req}`)
-  for (var i = 0; i < checks.length; i++){    
+  for (var i = 0; i < checks.length; i++) {
     let getCurrentColor = checks[i].style.backgroundColor
-    switch (getCurrentColor){
+    switch (getCurrentColor) {
       case "black":
         checks[i].style.backgroundColor = "white"
-        break;
-        case "white":
-          checks[i].style.backgroundColor = "black"
-          break;
-          default:
-            checks[i].style.backgroundColor = "black"
+        break
+      case "white":
+        checks[i].style.backgroundColor = "black"
+        break
+      default:
+        checks[i].style.backgroundColor = "black"
     }
   }
 }
